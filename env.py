9c282fdf9a3d4ae62a702c91f63f99775861650d
Maras00 <marcinmasiarek@gmail.com>
2021-01-12 13:23:10 +0000

    Connect Flask to MongoDB

--- env.py
+++ env.py
@@ -3,5 +3,5 @@ import os
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "P+hxgt5hM_nRh=z*.D1hW+Y=I&#YGQ")
-os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://maras00:saram1979@myfirstcluster.tagyq.mongodb.net/cars_vintage?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "cars_vintage")
