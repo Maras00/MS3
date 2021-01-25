4a7c417d358a9df447d63d01f34c478edc76df9e
Maras00 <marcinmasiarek@gmail.com>
2021-01-25 15:00:55 +0000

    Added a carousel with images on cars.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+     $('#carousel-auto').carousel();
+  setInterval(function() {
+    $('#carousel-auto').carousel('next');
+  }, 2000);    
     $('select').formSelect();
+     $('.carousel').carousel({
+    fullWidth: false,
+    indicators: true,
+    pressed: true,
+    onCycleTo: null
+  });
   });
