e4e4429046d08c6d19cca2c35f235558f368cf01
Maras00 <marcinmasiarek@gmail.com>
2021-01-26 13:46:32 +0000

    Change colors in navbar, mobile navbar and categories

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
-     $('#carousel-auto').carousel();
-  setInterval(function() {
+    $('#carousel-auto').carousel();
+        setInterval(function() {
     $('#carousel-auto').carousel('next');
-  }, 2000);    
+        }, 2000);   
     $('select').formSelect();
      $('.carousel').carousel({
     fullWidth: false,
